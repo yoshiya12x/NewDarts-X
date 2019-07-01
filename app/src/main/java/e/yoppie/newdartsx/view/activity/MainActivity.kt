9b08a8519c08e01200747900fa446b759d91afea
yoshiya12x <hikowaiide@gmail.com>
2019-07-01 23:22:32 +0900

    edit emphasize animation

--- app/src/main/java/e/yoppie/newdartsx/view/activity/MainActivity.kt
+++ app/src/main/java/e/yoppie/newdartsx/view/activity/MainActivity.kt
@@ -1,24 +1,16 @@
 package e.yoppie.newdartsx.view.activity
 
-import android.arch.lifecycle.ViewModelProviders
-import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import e.yoppie.newdartsx.R
-import e.yoppie.newdartsx.databinding.ActivityMainBinding
 import e.yoppie.newdartsx.util.Animation
-import e.yoppie.newdartsx.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-
-        Animation.emphasize(this, binding.startButton)
-
         setContentView(R.layout.activity_main)
+        Animation.emphasize(this, start_button)
     }
 }
