d556bf3bc6815dab2c0ebc3b116195e2fcfe2060
yoshiya12x <hikowaiide@gmail.com>
2019-07-02 20:23:41 +0900

    add targetactivity

---
+++ app/src/main/java/e/yoppie/newdartsx/view/activity/TargetActivity.kt
@@ -0,0 +1,13 @@
+package e.yoppie.newdartsx.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import e.yoppie.newdartsx.R
+
+class TargetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_target)
+    }
+}
