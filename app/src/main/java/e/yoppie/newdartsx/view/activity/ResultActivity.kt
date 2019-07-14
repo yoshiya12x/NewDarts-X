5727151e9e638edb9fb0855e6b62a0e5123c0584
yoshiya12x <hikowaiide@gmail.com>
2019-07-14 23:54:57 +0900

    edit pie chart

--- app/src/main/java/e/yoppie/newdartsx/view/activity/ResultActivity.kt
+++ app/src/main/java/e/yoppie/newdartsx/view/activity/ResultActivity.kt
@@ -2,9 +2,16 @@ package e.yoppie.newdartsx.view.activity
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.jakewharton.rxbinding2.view.clicks
 import e.yoppie.newdartsx.R
 import e.yoppie.newdartsx.model.room.entity.SoundEntity
@@ -14,6 +21,7 @@ import e.yoppie.newdartsx.util.Sound
 import io.reactivex.Completable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_result.*
+import kotlin.collections.ArrayList
 
 class ResultActivity : AppCompatActivity() {
 
@@ -30,11 +38,13 @@ class ResultActivity : AppCompatActivity() {
         val bullCount = intent.getIntExtra("bullCount", 0)
         val inBullCount = intent.getIntExtra("inBullCount", 0)
         val allCount = intent.getIntExtra("allCount", 0)
+
         bullScoreTextView.text = "$bullCount/$allCount"
         inBullScoreTextView.text = "$inBullCount/$allCount"
         Animation.emphasize(this, againButton)
 
         initButtons()
+        initPie(bullCount, inBullCount, allCount)
     }
 
     @SuppressLint("CheckResult")
@@ -50,6 +60,36 @@ class ResultActivity : AppCompatActivity() {
             val intent = Intent(this, SelectWordActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun initPie(bullCount: Int, inBullCount: Int, allCount: Int){
+        pie.setUsePercentValues(true)
+        pie.centerText = "BULL RATE"
+        pie.legend.textColor = Color.WHITE
+        pie.legend.textSize = 16f
+
+//        val value = listOf(
+//            (bullCount / allCount).toFloat(),
+//            (inBullCount / allCount).toFloat(),
+//            ((bullCount + inBullCount) / allCount).toFloat()
+//        )
+        val value = listOf(10f, 20f, 30f)
+        val label = listOf("Bull", "InBull", "Others")
+        val entry = ArrayList<PieEntry>()
+        for(i in value.indices) {
+            entry.add( PieEntry(value[i], label[i]) )
+        }
+
+        val dataSet = PieDataSet(entry, "")
+        dataSet.colors = listOf(Color.rgb(3, 135, 236), Color.rgb(17, 79, 128), Color.rgb(46, 46, 46))
+        dataSet.setDrawValues(true)
+
+        val pieData = PieData(dataSet)
+        pieData.setValueFormatter(PercentFormatter())
+        pieData.setValueTextSize(20f)
+        pieData.setValueTextColor(Color.WHITE)
+
+        pie.data = pieData
     }
 
     @SuppressLint("CheckResult")
